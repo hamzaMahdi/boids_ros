b616594dcb81ad4b1659798e12a2fc48b94fcf4d
Hamza Mahdi <hamza.mahdi@uwaterloo.ca>
2019-11-11 18:51:05 -0500

    updated reynold's controller

--- cfg/create_robot_positions.py
+++ cfg/create_robot_positions.py
@@ -2,12 +2,12 @@
 # Please make sure to go back and manually add the path to the bitmap file
 file_name='my_empty_10x10_20.world'
 f = open("../resources/sim/"+file_name,"w+")
-x = 0.0
-y = 0.0
+x = -1.5
+y = -0.1
 for i in range(2):
 	for j in range(10):
 		f.write('sphero( pose [ %f %f 0.000 0.000 ] name "sphero_%d" color "blue")\n'%(x,y,i*10+j))
 		x -= 0.2
 	y += 0.2
-	x = 0.0
+	x = -1.5
 f.close()
